4b47a02f266820dc9161bd08715c9e7e2dab27af
Callan <callanjwatson@gmail.com>
2024-04-15 16:07:43 +0800

    addedtext fields to create prompt

--- components/Form.jsx
+++ components/Form.jsx
@@ -3,7 +3,28 @@ import Link from 'next/link';
 const Form = ({type, post, setPost, submitting, handleSubmit}) => {
   return (
     <section className='w-full max-w-full flex-start flex-col'>
-      <h1 className='head_text text-left'>{type} Post</h1>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradiesnt'>{type} Post</span>
+      </h1>
+      <p className='desc text-left max-w-md'>{type} a prompt bossman
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'>
+          <label>
+            <span className='font-satoshi'>
+              your ai Prompt
+            </span>
+
+            <textarea
+              value={post.prompt}
+              onChange={(e) => setPost({...post,
+              prompt: e.target.value})}
+              placeholder='Enter Prompt'
+              required
+              classNamw ="form_textarea"></textarea>
+          </label>
+      </form>
     </section>
   )
 }
